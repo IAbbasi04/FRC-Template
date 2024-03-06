41fb248039457c12e04025bc06f8f826f6233ca1
Ibrahim Abbasi <Ibrahimzabbasi04@gmail.com>
2024-03-05 22:54:29 -0500

    Added 8592 swerve library

---
+++ src/main/java/frc/robot/modules/DriveModule.java
@@ -0,0 +1,16 @@
+package frc.robot.modules;
+
+import frc.robot.common.Enums.MatchMode;
+
+public class DriveModule extends Module {
+
+    @Override
+    public void init(MatchMode mode) {
+
+    }
+
+    @Override
+    public void periodic() {
+    
+    }
+}
